ac4c0166a3f38c69c9e0c8ef56da3ff697e9c966
Alao Olamide <olamidealao22@gmail.com>
2025-01-22 19:24:26 +0100

    update model

--- src/docs/swaggerDocs.js
+++ src/docs/swaggerDocs.js
@@ -4,15 +4,12 @@
 const swaggerDocumentation = {
     openapi: '3.0.0',
     info: {
-      title: 'Remitex API',
+      title: 'Example API',
       version: '1.0.0',
-      description: 'API documentation for the Remitex financial project',
+      description: 'API documentation for the  example project',
     },
     servers: [
-      {
-        url: 'https://remitex-server.onrender.com',
-        // description: 'Development Server',
-      },
+   
       {
         url: 'http://localhost:5000',
         // description: 'Development Server',
